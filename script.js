bedf5ad5db415da6daf19bc4786b3033a3d56325
sylvainmahetest <sylvainmahe4815@gmail.com>
2026-04-10 22:18:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -228,63 +228,7 @@ function event()
     });
     
     //////////////////////////////////////////////////////////////////////////////////////////
-    let index = 0;
-    
-    for (index = 0; index < _countTagMediaImg; index++)
-    {
-        const INDEX = index;
-        
-        _tagMedia[INDEX].addEventListener("touchstart", event =>
-        {
-            const LENGTH_TOUCH = event.touches.length;
-            
-            if (LENGTH_TOUCH === 1)
-            {
-                clientY = event.touches[0].clientY;
-                clientYPrevious = clientY;
-            }
-            else if (LENGTH_TOUCH === 2)
-            {
-                clientY = (event.touches[0].clientY + event.touches[1].clientY) * 0.5;
-                clientYPrevious = clientY;
-            }
-            
-            event.preventDefault();
-        },
-        {
-            passive: false
-        });
-        
-        _tagMedia[INDEX].addEventListener("touchmove", event =>
-        {
-            const LENGTH_TOUCH = event.touches.length;
-            
-            if (LENGTH_TOUCH === 1)
-            {
-                clientY = event.touches[0].clientY;
-                
-                deltaY = clientY - clientYPrevious;
-                clientYPrevious = clientY;
-            }
-            else if (LENGTH_TOUCH === 2)
-            {
-                clientY = (event.touches[0].clientY + event.touches[1].clientY) * 0.5;
-                
-                deltaY = clientY - clientYPrevious;
-                clientYPrevious = clientY;
-            }
-            
-            if (deltaY < 0 || deltaY > 0)
-            {
-                _eventScroll += deltaY * 1.5;
-            }
-            
-            event.preventDefault();
-        },
-        {
-            passive: false
-        });
-    }
+  
     //////////////////////////////////////////////////////////////////////////////////////////
     
     //_tagCanvasParticule.addEventListener("wheel", event =>
